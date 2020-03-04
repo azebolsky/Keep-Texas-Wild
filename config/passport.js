ddc43fce33d8c2f986bf2f5525e911de0c3d465c
ADAM <zebolsky.adam@gmail.com>
2020-03-03 23:00:10 -0600

    require Profile at passport

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const User = require('../models/user');
+const Profile = require('../models/profile');
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
@@ -19,9 +20,18 @@ passport.use(new GoogleStrategy({
               email: profile.emails[0].value,
               googleId: profile.id
           });
+          const profile = new Profile({
+              firstName: profile.displayName,
+              email: profile.emails[0].value,
+              googleId: profile.id
+          });
           newUser.save(function(err) {
               if (err) return cb(err);
               return cb(null, newUser);
+          });
+          profile.save(function(err) {
+            if (err) return cb(err);
+            return cb(null, profile);
           });
         }
     });
