549e6ba2be1ffdb43d4bf4a63d21a46d2f245f0b
ADAM <zebolsky.adam@gmail.com>
2020-05-06 17:02:25 -0500

    clean up code

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -7,36 +7,35 @@ module.exports = {
 };
 
 function deleteReview(req, res) {
-    Parks.findOne({'reviews._id': req.params.id}, function(err, park) {
+    Parks.findOne({ 'reviews._id': req.params.id }, function (err, park) {
         const reviewSubdoc = park.reviews.id(req.params.id);
-        if(!reviewSubdoc.createdBy.equals(req.user && req.user.id)) return res.redirect(`/parks/${park._id}`);
+        if (!reviewSubdoc.createdBy.equals(req.user && req.user.id)) return res.redirect(`/parks/${park._id}`);
         reviewSubdoc.remove();
-        park.save(function(err) {
+        park.save(function (err) {
             res.redirect(`/parks/${park._id}`);
         });
     });
 }
 
 function update(req, res) {
-    Parks.findOne({'reviews._id': req.params.id}, function(err, park) {
+    Parks.findOne({ 'reviews._id': req.params.id }, function (err, park) {
         const reviewSubdoc = park.reviews.id(req.params.id);
-        if(!reviewSubdoc.createdBy.equals(req.user && req.user.id)) return res.redirect(`/parks/${park._id}`);
+        if (!reviewSubdoc.createdBy.equals(req.user && req.user.id)) return res.redirect(`/parks/${park._id}`);
         req.body.createdBy = req.user._id;
         reviewSubdoc.content = req.body.content;
         reviewSubdoc.rating = req.body.rating;
-        park.save(function(err) {
+        park.save(function (err) {
             res.redirect(`/parks/${park._id}`);
         });
     });
 }
 
 function create(req, res) {
-    Parks.findById(req.params.id, function(err, park) {
+    Parks.findById(req.params.id, function (err, park) {
         req.body.createdBy = req.user._id;
         req.body.userName = req.user.name;
-        console.log(req.body.userName);
         park.reviews.push(req.body);
-        park.save(function(err) {
+        park.save(function (err) {
             res.redirect(`/parks/${park._id}`);
         });
     });
